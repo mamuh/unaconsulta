a3bf0bbee41f76f71fb17870998e7441d563341d
Victor Capelli <victorbustocapelli@hotmail.com>
2020-06-08 15:57:53 -0300

    Review controller done

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,45 @@
+class ReviewsController < ApplicationController
+
+  before_action :set_review, only: [:edit, :destroy]
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @property = Property.find(params[:property.id])
+    @review.property = @property
+    @review.user = current_user
+    if @review.save
+      redirect_to @property
+    else
+      flash[:alert] = "Please fill in all fields"
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @review.update(review_params)
+    redirect_to @property
+  end
+
+  def destroy
+    @review.destroy
+    redirect_to @properties_path
+  end
+
+  private
+
+  def set_review
+    @review = Review.find(params[:id])
+  end
+
+  def review_params
+    params.require(:reviews).permit(:content, :stars, :checkin, :checkout)
+  end
+
+end
