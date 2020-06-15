b639d6760ef42ad12e4361ac39d77bba3fd00a8a
Mariana Caramori <musardo@mail.com>
2020-06-15 12:12:55 -0300

    modal now closes with escape key

---
+++ app/javascript/plugins/init_modal.js
@@ -0,0 +1,30 @@
+const initModal = () => {
+  var modal = document.getElementById("myModal");
+
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+  var imgs = document.querySelectorAll(".small-pic");
+  var modalImg = document.getElementById("img01");
+
+  imgs.forEach(img => {
+    img.onclick = function(){
+      modal.style.display = "block";
+      modalImg.src = this.src;
+    }
+  })
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  window.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+      modal.style.display = "none";
+    }
+  })
+}
+
+export { initModal };
