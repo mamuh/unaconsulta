77cefb681271ac010331bdc745e91fa90c4955c1
Mariana Caramori <musardo@mail.com>
2020-06-15 12:25:16 -0300

    all review images are clickable

--- app/javascript/plugins/init_modal.js
+++ app/javascript/plugins/init_modal.js
@@ -15,7 +15,7 @@ const initModal = () => {
   // Get the <span> element that closes the modal
   var span = document.getElementsByClassName("close")[0];
 
-  // When the user clicks on <span> (x), close the modal
+  // close the modal
   span.onclick = function() {
     modal.style.display = "none";
   }
