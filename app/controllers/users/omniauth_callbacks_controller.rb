637ab14245e7491becbe5a100daaec056cabb705
Mariana Caramori <musardo@mail.com>
2020-06-15 15:01:11 -0300

    facebook login is now functional with profile pic

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,11 +3,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     user = User.find_for_facebook_oauth(request.env['omniauth.auth'])
 
     if user.persisted?
-    sign_in_and_redirect user, event: :authentication
-    set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
+      sign_in_and_redirect user, event: :authentication
+      set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
     else
-    session['devise.facebook_data'] = request.env['omniauth.auth']
-    redirect_to new_user_registration_url
+      session['devise.facebook_data'] = request.env['omniauth.auth']
+      redirect_to new_user_registration_url
     end
   end
 end
