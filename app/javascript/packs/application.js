c85841c66ff615de19de16516c02277daa17bc5a
Mariana Caramori <musardo@mail.com>
2020-06-16 14:30:08 -0300

    x

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require('flatpickr/dist/flatpickr.min.css')
+// require('flatpickr/dist/flatpickr.min.css')
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
--- app/javascript/plugins/init_flatpickr.js
+++ app/javascript/plugins/init_flatpickr.js
@@ -1,5 +1,5 @@
 // First we define two variables that are going to grab our inputs field. You can check the ids of the inputs with the Chrome inspector.
-
+// import 'flatpickr/dist/themes/dark.css';
 import flatpickr from "flatpickr";
 
 
