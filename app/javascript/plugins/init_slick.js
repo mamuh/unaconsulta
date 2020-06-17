e9d3546ee232d6c226f9a62e4619f677dbd00a3d
gabrielloppes <gabriellopees@hotmail.com>
2020-06-17 18:18:50 -0300

    Added slider

---
+++ app/javascript/plugins/init_slick.js
@@ -0,0 +1,25 @@
+import $ from 'jquery';
+import 'slick-carousel';
+
+// const reviews = document.querySelectorAll('.card-review');
+
+const slick = () => {
+    $(document).ready(function(){
+      $('.property-photos').slick({
+        slidesToShow: 3,
+        dots:true,
+        arrows: true,
+        centerMode: true,
+        focusOnSelect: true
+      });
+      // $('.review-content-container').slick({
+      //   dots: true,
+      //   vertical: true,
+      //   slidesToScroll: reviews.length,
+      //   slidesToShow: 1,
+      //   verticalSwiping: true,
+      // });
+    });
+}
+
+export { slick };
